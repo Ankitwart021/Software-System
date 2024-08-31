69dd997784ec33954b72f72ae931a7085a3fc9e4
Ankitwart021 <wartankit006@gmail.com>
2024-08-31 22:48:33 +0530

    Add files via upload

---
+++ 1a.c
@@ -0,0 +1,17 @@
+// Ankit Kumar
+// MT2024021
+//31-08-24
+/*
+	Create the following types of a files using (i) shell command (ii) system call
+	a.soft link(symlink system call)
+*/
+#include<stdio.h>
+#include<unistd.h>
+	int main(int argc, char *argv[]){
+	//int o= symlink("/home/darshit/Desktop/Software_Sysytem/SoftlinkSource.txt","/home/darshit/Desktop/Software_Sysytem/Softlink.txt");
+	int o= symlink(argv[1],argv[2]);
+	if(o==0){
+		printf("SoftLink is created succsesfully");
+	}
+	return 0;
+}
